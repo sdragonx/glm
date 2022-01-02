73d02e32583febf680d992ceb4c4357842efadc3
中華大帝 <sdragonx@foxmail.com>
2022-01-02 17:07:14 +0800

    update 2022-1-2

---
+++ glm/gtx/normal.hpp
@@ -0,0 +1,29 @@
+/*
+ Copyright (c) 2005-2020 sdragonx (mail:sdragonx@foxmail.com)
+
+ normal.hpp
+
+ 2021-12-31 17:19:36
+
+*/
+#ifndef GLM_NORMAL_HPP_20211231171936
+#define GLM_NORMAL_HPP_20211231171936
+
+#include "../vec.hpp"
+
+namespace glm{
+
+//
+// vec3 triangleNormal( in vec3, in vec3, in vec3 )
+//
+
+template<typename T>
+inline vec<3, T> triangleNormal(const vec<3, T>& a, const vec<3, T>& b, const vec<3, T>& c)
+{
+    //return normalize(cross(a - b, a - c));
+    return normalize(cross(b - a, c - a));
+}
+
+}// end namespace glm
+
+#endif// GLM_NORMAL_HPP_20211231171936
---
+++ glm/gtx/vector_angle.hpp
@@ -0,0 +1,20 @@
+/*
+ Copyright (c) 2005-2020 sdragonx (mail:sdragonx@foxmail.com)
+
+ vector_angle.hpp
+
+ 2022-01-01 01:42:56
+
+*/
+#ifndef GLM_VECTOR_ANGLE_HPP_20220101014256
+#define GLM_VECTOR_ANGLE_HPP_20220101014256
+
+#include "../detail/setup.hpp"
+
+namespace glm{
+
+
+
+}// end namespace glm
+
+#endif// GLM_VECTOR_ANGLE_HPP_20220101014256
